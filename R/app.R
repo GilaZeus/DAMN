64e3b5612db5beeb7581b275eb20de4e665a5ae3
GilaZeus <gilyazevr@gmail.com>
2021-08-22 06:04:09 +0200

    Fix problems

--- R/app.R
+++ R/app.R
@@ -1,22 +1,20 @@
-library(shiny)
-library(shinyFiles)
-
 # Define UI ----
-ui <- fluidPage(
-  titlePanel("DAMN"),
-  sidebarLayout(
-    sidebarPanel(
-      textInput(
+ui <- shiny::fluidPage(
+  shiny::titlePanel("DAMN"),
+  shiny::sidebarLayout(
+    shiny::sidebarPanel(
+      shiny::textInput(
         "extension",
         "Enter input data extension:",
         value = "tab"
       ),
-      shinyDirButton(
+      shinyFiles::shinyDirButton(
         "dir",
         "Select a folder",
-        "Upload"
+        "Upload",
+        FALSE
       ),
-      radioButtons(
+      shiny::radioButtons(
         "step",
         "Select normalization step:",
         c("Raw" = "raw",
@@ -27,33 +25,77 @@ ui <- fluidPage(
           "Distribution normalization" = "distr")
       )
     ),
-    mainPanel(
-      h2("Bar chart(s)", align = "center"),
-      plotOutput("barchart"))
+    shiny::mainPanel(
+      shiny::h2("Bar chart(s)", align = "center"),
+      shiny::plotOutput("barchart"))
   )
 )
 
 # Define server logic ----
 server <- function(input, output) {
-  root_dir = c(wd = if(.Platform$OS.type == "unix") '/' else "C:")
-  shinyDirChoose(
+  root_dir = c(wd = if(.Platform$OS.type == "unix") '/' else "C:/")
+  shinyFiles::shinyDirChoose(
     input,
     'dir',
     roots = root_dir
   )
-  output$barchart <- renderPlot({
-    directory <- parseDirPath(root_dir, input$dir)
+  output$barchart <- shiny::renderPlot({
+    directory <- shinyFiles::parseDirPath(root_dir, input$dir)
     switch(
       input$step,
-      "raw" = ggplot(data = merge_datasets(directory = directory, file_ext = input$extension, step = "raw"), aes(x = CH1, y = CH2, color = name)) + geom_point() + facet_wrap(~ name),
-      "maplot" = ggplot(data = mean_average(merge_datasets(directory = directory, file_ext = input$extension, step = "raw")), aes(x = A, y = M, color = name)) + geom_point() + facet_wrap(~ name),
-      "norm" = ggplot(data = normalize(mean_average(merge_datasets(directory = directory, file_ext = input$extension, step = "raw"))), aes(x = A, y = M, color = name)) + geom_point() + facet_wrap(~ name),
-      "cent" = ggplot(data = center_norm(merge_datasets(directory = directory, file_ext = input$extension)), aes(x = name, y = M, color = name)) + geom_boxplot(varwidth = TRUE) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)),
-      "scale" = ggplot(data = scale_norm(center_norm(merge_datasets(directory = directory, file_ext = input$extension))), aes(x = name, y = M, color = name)) + geom_boxplot(varwidth = TRUE) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)),
-      "distr" = ggplot(data = distribution_norm(scale_norm(center_norm(merge_datasets(directory = directory, file_ext = input$extension)))), aes(y = M)) + geom_boxplot(varwidth = TRUE) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      "raw" = ggplot2::ggplot(
+        data = merge_datasets(directory = directory, file_ext = input$extension, step = "raw"),
+        ggplot2::aes(x = CH1, y = CH2, color = name)
+        ) +
+        ggplot2::geom_point() +
+        ggplot2::facet_wrap(~ name),
+      "maplot" = ggplot2::ggplot(
+        data = mean_average(merge_datasets(directory = directory, file_ext = input$extension, step = "raw")),
+        ggplot2::aes(x = A, y = M, color = name)
+        ) +
+        ggplot2::geom_point() +
+        ggplot2::facet_wrap(~ name),
+      "norm" = ggplot2::ggplot(
+        data = normalize(mean_average(merge_datasets(directory = directory, file_ext = input$extension, step = "raw"))),
+        ggplot2::aes(x = A, y = M, color = name)
+        ) +
+        ggplot2::geom_point() +
+        ggplot2::facet_wrap(~ name),
+      "cent" = ggplot2::ggplot(
+        data = center_norm(merge_datasets(directory = directory, file_ext = input$extension)),
+        ggplot2::aes(x = name, y = M, color = name)
+        ) +
+        ggplot2::geom_boxplot(varwidth = TRUE) +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)),
+      "scale" = ggplot2::ggplot(
+        data = scale_norm(center_norm(merge_datasets(directory = directory, file_ext = input$extension))),
+        ggplot2::aes(x = name, y = M, color = name)
+        ) +
+        ggplot2::geom_boxplot(varwidth = TRUE) +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)),
+      "distr" = ggplot2::ggplot(
+        data = distribution_norm(scale_norm(center_norm(merge_datasets(directory = directory, file_ext = input$extension)))),
+        ggplot2::aes(y = M)
+        ) +
+        ggplot2::geom_boxplot(varwidth = TRUE) +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
     )
   })
 }
 
 # Run the app ----
-shinyApp(ui = ui, server = server)
+# shiny::shinyApp(ui = ui, server = server)
+
+
+#' Run App
+#'
+#' Run Shiny App for more comfortable plotting.
+#'
+#' @param gui ui argument of a shiny app.
+#' @param gserver server argument of a shiny app.
+#' @export
+run_app <- function(gui = ui, gserver = server) {
+  shiny::shinyApp(ui = gui, server = gserver)
+}
+
+run_app()
--- R/normalization.R
+++ R/normalization.R
@@ -1,8 +1,9 @@
 #' File preparation
-#' 
+#'
 #' Prepare a single microarray dataset for plotting.
 #' Reading data and selecting columns of interest.
-#' 
+#'
+#' @importFrom magrittr %>%
 #' @param file_name Name of a delimited file containing
 #'                  at least SPOT, CH1B, CH1I, CH2I and
 #'                  CH2B columns.
@@ -19,9 +20,10 @@ prepare_single_microarray <- function(file_name) {
 
 
 #' Remove noise
-#' 
+#'
 #' Subtract background noise from every channel.
-#' 
+#'
+#' @importFrom magrittr %>%
 #' @param microarray A tibble containing at least SPOT, CH1B,
 #'                   CH1I, CH2I, CH2B and name columns.
 #' @return Tibble containing input columns and two
@@ -33,19 +35,20 @@ subtract_background <- function(microarray) {
                           CH1 = CH1I - CH1B,
                           CH2 = CH2I - CH2B
   )
-  
+
   # Extinction < 0 does not make sense; such values must be removed.
   result$CH1[result$CH1 <= 0] <- 1
   result$CH2[result$CH2 <= 0] <- 1
-  
+
   return(result)
 }
 
 
 #' Transform channels
-#' 
+#'
 #' Add (M)ean and (A)verage to a tibble containing CH1 and CH2 columns.
-#' 
+#'
+#' @importFrom magrittr %>%
 #' @param microarray A tibble containing at least CH1, CH2 and name columns.
 #' @return Same table with two new columns: M(ean) and A(verage).
 #' @export
@@ -54,13 +57,14 @@ mean_average <- function(microarray) {
     dplyr::mutate(M = log2(CH2) - log2(CH1),
                   A = (log2(CH2) + log2(CH1)) / 2) %>%
     dplyr::ungroup() %>% return()
-}  
+}
 
 
 #' Normalization
-#' 
+#'
 #' Normalize mean and average.
 #'
+#' @importFrom magrittr %>%
 #' @param mocroarray A table containing at least M, A and name columns.
 #' @return Same table normalized and with NA removed.
 #' @export
@@ -68,7 +72,7 @@ normalize <- function(microarray) {
   microarray %>% dplyr::group_by(name) %>%
     dplyr::mutate(M = M - (coef(lm(M ~ A))[1] + coef(lm(M ~ A))[2] * A)) %>%
     dplyr::ungroup() %>% na.omit() %>% return()
-  
+
   # lm1 <- lm(microarray$M ~ microarray$A)
   # microarray$M <- microarray$M - (coef(lm1)[1] + coef(lm1)[2] * microarray$A)
   # microarray %>% na.omit() %>% return()
@@ -76,9 +80,10 @@ normalize <- function(microarray) {
 
 
 #' Purge
-#' 
+#'
 #' Remove all columns except M, name and SPOT.
-#' 
+#'
+#' @importFrom magrittr %>%
 #' @param microarray A tibble containing M, name and SPOT columns.
 #' @return A tibble containing only  M, name and SPOT columns.
 #' @export
@@ -88,11 +93,12 @@ purge <- function(microarray) {
 
 
 #' Merge datasets
-#' 
+#'
 #' Merge *.tab datasets from a directory. Safe them as tibble with
 #' M, name and SPOT columns. Secure switching back to the user-defined
 #' file directory.
-#' 
+#'
+#' @importFrom magrittr %>%
 #' @param directory=getwd() Working directory with delimited files.
 #' @param file_ext="tab" File extension of delimited files containing
 #'                       M, name and SPOT columns.
@@ -104,7 +110,7 @@ purge <- function(microarray) {
 merge_datasets <- function(directory = getwd(), file_ext = "tab", step = "between") {
   old_directory <- getwd()
   setwd(directory)
-  
+
   # Join datasets.
   if(step == "between") {
     result <- tibble::tibble(M = NA,
@@ -130,22 +136,23 @@ merge_datasets <- function(directory = getwd(), file_ext = "tab", step = "betwee
       buffer <- dataset %>%
         prepare_single_microarray() %>%
         subtract_background() %>%
-        select(CH1, CH2, name, SPOT)
+        dplyr::select(CH1, CH2, name, SPOT)
       result <- dplyr::add_row(result, buffer)
     }
-  }  
+  }
   # Switch back to the user-defined file directory.
   setwd(old_directory)
-  
+
   return(na.omit(result))
 }
 
 
 #' Centering
-#' 
+#'
 #' Centering step of between array normalization.
 #' Subtract median from each value, so the medians become 0.
-#' 
+#'
+#' @importFrom magrittr %>%
 #' @param microarrays A tibble containing M and name columns.
 #' @return Same tibble with M column centered.
 #' @export
@@ -159,10 +166,11 @@ center_norm <- function(microarrays) {
 
 
 #' Scaling
-#' 
+#'
 #' Scaling step of between array normalization.
 #' Divide each value through MAD, so std. deviations become 1.
-#' 
+#'
+#' @importFrom magrittr %>%
 #' @param microarrays A tibble containing M and name columns.
 #' @return Same tibble with  M column scaled.
 #' @export
@@ -176,26 +184,20 @@ scale_norm <- function(microarrays) {
 
 
 #' Distribution normalization
-#' 
+#'
 #' Sort all values in ascending order, build a mean value for each
 #' position in the order, so distributions of all experiments become
 #' the same.
-#' 
+#'
+#' @importFrom magrittr %>%
 #' @param microarrays A tibble containing M and name columns.
 #' @return A new tibble with "number" and M columns, M column
 #'         is normalized.
 #' @export
 distribution_norm <- function(microarrays) {
   microarrays %>% dplyr::arrange(M) %>% dplyr::group_by(name) %>%
-    dplyr::mutate(number = row_number()) %>%
+    dplyr::mutate(number = dplyr::row_number()) %>%
     dplyr::ungroup() %>% dplyr::group_by(number) %>%
     dplyr::summarise(M = mean(M)) %>% return()
 }
 
-#' Run App
-#' 
-#' Run Shiny App for more comfortable plotting.
-#' @export
-run_app <- function() {
-  shiny::runApp()
-}
